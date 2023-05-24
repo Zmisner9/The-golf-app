8f6fbb66e079262ef5d2f85eef5d9dc4d009eb1f
Zach Misner <zmisner5298@stu.d214.org>
2023-05-24 09:20:04 -0500

    hey

--- The golf app/ContentView.swift
+++ The golf app/ContentView.swift
@@ -1,24 +1,71 @@
-//
-//  ContentView.swift
-//  The golf app
-//
-//  Created by Zachary E. Misner on 4/24/23.
-//
-
 import SwiftUI
+import UIKit
 
-struct ContentView: View {
-    @Environment(\.openURL) var openURL
+   
+
+
+struct GolfButtonLayout: View {
+    let links: [String] = [
+        "https://www.youtube.com/watch?v=xG66WXfnCTg", // Slice improvement video link
+        "https://www.youtube.com/watch?v=EndU64gWnAQ", // Hook improvement video link
+        "https://www.youtube.com/watch?v=On7K89a1Buw", // Top improvement video link
+        "https://www.youtube.com/watch?v=ihWxNa8gUgY", // Chunk improvement video link
+        "https://www.youtube.com/watch?v=EubxU2vLM2s"  // Shank improvement video link
+    ]
+    
     var body: some View {
-        Button("Visit Apple") {
-        openURL(URL (string: "https://www.apple.com")!)
+        VStack {
+            Button(action: {
+                // Open the Slice improvement video link
+                openLink(url: links[0])
+            }) {
+                Text("Slice Fix")
+            }
+            Button(action: {
+                // Open the Hook improvement video link
+                openLink(url: links[1])
+            }) {
+                Text("Hook Fix")
+            }
+            Button(action: {
+                // Open the Top improvement video link
+                openLink(url: links[2])
+            }) {
+                Text("Top Fix")
+            }
+            Button(action: {
+                // Open the Chunk improvement video link
+                openLink(url: links[3])
+            }) {
+                Text("Chunk Fix")
+            }
+            Button(action: {
+                // Open the Shank improvement video link
+                openLink(url: links[4])
+            }) {
+                Text("Shank Fix")
+            }
         }
-        Text("Hope you arae having fun in England")
+    }
+    
+    func openLink(url: String) {
+        guard let url = URL(string: url) else {
+            return // Handle invalid URL case
+        }
+        UIApplication.shared.open(url)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct GolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GolfButtonLayout()
+        }
+    }
+}
+
+struct GolfApp_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        GolfButtonLayout()
     }
 }
--- The golf app/The_golf_appApp.swift
+++ The golf app/The_golf_appApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct The_golf_appApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GolfButtonLayout()
         }
     }
 }
